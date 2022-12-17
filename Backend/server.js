2a03d4e39c205ebc59a9446e8e4b8a8e7b702a63
Faizankhan99 <usmani.faizan98@gmail.com>
2022-12-17 14:41:47 +0530

    add

--- Backend/server.js
+++ Backend/server.js
@@ -5,15 +5,18 @@ const Job=require("./Routing/Routing")
 require("dotenv").config()
 const app = express()
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.json())
-
 app.use(cors())
+require("dotenv").config()
+const PORT = process.env.PORT || 9000;
+const mongo_url=process.env.mongo_url||""
+
+
 app.get('/', (req, res) => res.send('hello'))
 
 app.use("/job", Job)
 
-app.listen(8080, async () => {
-    await mongoose.connect("mongodb+srv://faizan:faizan@cluster0.hnanw0r.mongodb.net/Users")
+app.listen(PORT, async () => {
+    await mongoose.connect(mongo_url)
     console.log('server started on port 8080')
 })
