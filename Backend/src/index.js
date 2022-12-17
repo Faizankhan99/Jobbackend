159cb8324ea089fae4fc1c2e9571b6d58d463424
Faizankhan99 <usmani.faizan98@gmail.com>
2022-12-17 14:51:13 +0530

    add

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { mongoose } = require('mongoose')
 const cors = require("cors")
-const Job=require("./Routing/Routing")
+const Job=require("../Routing/Routing")
 require("dotenv").config()
 const app = express()
 app.use(express.urlencoded({ extended: true }))
